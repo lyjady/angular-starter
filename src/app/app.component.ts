c71774bca1489f7a57f4326cb8cadd3230859bce
991296755@qq.com <lyj112233>
2021-03-12 14:55:40 +0800

    新增

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {HeroService} from './modules/hero/service/hero.service';
+import {AccountService} from './services/acount.service';
 import {NavigationStart, Router} from '@angular/router';
 import {filter, map, switchMap} from 'rxjs/operators';
-import {ContextService} from './services/context.service';
+import {UserService} from './services/user.service';
+import {TOKEN} from './config/constant';
+import {EMPTY} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +17,20 @@ export class AppComponent {
 
   title = 'Angular-Starter'
 
-  constructor(private contextService: ContextService, private router: Router) {
+  constructor(private accountService: AccountService, private userService: UserService, private router: Router) {
     this.router.events.pipe(
       filter(event => event instanceof NavigationStart),
-      switchMap(() => this.contextService.setContext()),
-    ).subscribe()
+      switchMap(() => this.userService.user$),
+      switchMap(user => {
+        const token = localStorage.getItem(TOKEN)
+        if (!user && token) {
+          return this.accountService.account()
+        }
+        return EMPTY
+      })
+    ).subscribe(({user, token}) => {
+        localStorage.setItem(TOKEN, token)
+        this.userService.setUser(user)
+    })
   }
 }
--- src/app/guard/hero.guard.ts
+++ src/app/guard/hero.guard.ts
@@ -3,31 +3,31 @@ import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Route
 import {Observable, of} from 'rxjs';
 import {UserService} from '../services/user.service';
 import {switchMap} from 'rxjs/operators';
-import {ContextService} from '../services/context.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroGuard implements CanActivate {
 
-  constructor(private userService: UserService, private contextService: ContextService, private router: Router) {
+  constructor(private userService: UserService, private router: Router) {
   }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    this.userService.redirectUrl = state.url
     const auths: string[] = next.data.auths
-    return this.contextService.setContext().pipe(
-      switchMap(value => {
-        if (value) {
-          if (auths.includes(value.role)) {
+    return this.userService.user$.pipe(
+      switchMap(user => {
+        if (!user) {
+          this.router.navigate(['/login']).then(() => alert('您还未登录请先登录'))
+          return of(false)
+        } else {
+          if (auths.includes(user.role)) {
             return of(true)
           } else {
             this.router.navigate(['/home/hero/list']).then(() => alert('您没有操作权限，请联系管理员'))
             return of(false)
           }
         }
-        this.userService.redirectUrl = state.url
-        this.router.navigate(['/login']).then(() => alert('您还未登录请先登录'))
-        return of(false)
       })
     )
   }
